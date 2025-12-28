55dd375b341f8a3a22f989d4b3a4b7e7326acc8b
Kouchih Douae <douaekouchih2020il.com>
2025-12-28 13:10:22 +0100

    Initialisation Backend : Structure MVC + Service fonctionnelle

---
+++ services/RecommendationService.js
@@ -0,0 +1,8 @@
+// C'est ici que l'Étudiante A travaillera sur Neo4j
+class RecommendationService {
+    async getMatches(userId) {
+        // Logique de l'algorithme à venir
+        return [];
+    }
+}
+module.exports = new RecommendationService();

6964066f3a938c6e75a35da16d1996fa4c8aea34
rania-532 <bikikrerania@gmail.com>
2025-12-28 20:21:15 +0100

    chore: structure cleanup

--- services/RecommendationService.js
+++
@@ -1,8 +0,0 @@
-// C'est ici que l'Étudiante A travaillera sur Neo4j
-class RecommendationService {
-    async getMatches(userId) {
-        // Logique de l'algorithme à venir
-        return [];
-    }
-}
-module.exports = new RecommendationService();
